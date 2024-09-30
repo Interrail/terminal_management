81c0b946ecf59aef0b9d69227614f0facb787964
izzat1998 <izzattilla706@gmail.com>
2024-09-30 18:56:10 +0500

    github action test

--- terminal_management/settings/base.py
+++ terminal_management/settings/base.py
@@ -146,3 +146,6 @@ SPECTACULAR_SETTINGS = {
     "SERVE_INCLUDE_SCHEMA": False,
     # OTHER SETTINGS
 }
+print(os.getenv("DATABASE_URL"))
+print(os.getenv("DJANGO_SECRET_KEY"))
+print(os.getenv("DJANGO_SETTINGS_MODULE"))
