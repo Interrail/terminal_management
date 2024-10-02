8cee61793bdb883bdd21a1aa0dbe4da66820e39f
izzat1998 <izzattilla706@gmail.com>
2024-10-02 17:30:53 +0500

    debug false, urls change

--- terminal_management/urls.py
+++ terminal_management/urls.py
@@ -38,5 +38,6 @@ urlpatterns = [
     path("cdn/", include("apps.cdn.urls")),
 ]
 urlpatterns += [path("silk/", include("silk.urls", namespace="silk"))]
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
